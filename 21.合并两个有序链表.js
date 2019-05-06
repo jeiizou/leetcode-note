4564c9559178d191e2542659bd289dab4ee69aaa
j_pc <997282173@qq.com>
2019-05-06 19:43:26 +0800

    阿斯顿

--- 21.合并两个有序链表.js
+++ 21.合并两个有序链表.js
@@ -15,24 +15,38 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
+
 var mergeTwoLists = function(l1, l2) {
-    function ListNode(val) {
-        this.val = val;
-        this.next = null;
+    if (!l1) {
+        return l2;
+    }
+    if (!l2) {
+        return l1;
     }
 
-    let resnode=ListNode(l1.val);
-
-    function ReadNode(node){
-
-        let value=node.val;
-        
-        
-
-
-
-        if(node.next){
-            ReadNode(node.next);
+    let node1 = l1;
+    let node2 = l2;
+    let resNode = l1.val <= l2.val ? l1 : l2;
+    let head = resNode;
+    while (node1 || node2) {
+        let curNode;
+        if (!node1) {
+            curNode = node2;
+            node2 = node2.next;
+        } else if (!node2) {
+            curNode = node1;
+            node1 = node1.next;
+        } else {
+            if (node1.val <= node2.val) {
+                curNode = node1;
+                node1 = node1.next;
+            } else {
+                curNode = node2;
+                node2 = node2.next;
+            }
         }
+        resNode.next = curNode;
+        resNode = resNode.next;
     }
+    return head;
 };
