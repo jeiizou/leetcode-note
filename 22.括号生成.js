a29da5be6d86d570e8b9cf16d8660fa8efa3e865
ZHOUJIE <zhoujie01@souche.com>
2020-01-14 19:20:34 +0800

    demo

---
+++ 22.括号生成.js
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=22 lang=javascript
+ *
+ * [22] 括号生成
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+var generateParenthesis = function (n) {
+    let stack = [];
+    let count = n * 2;
+
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        
+    }
+};
+// @lc code=end
+
