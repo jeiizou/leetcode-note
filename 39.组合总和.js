402b89e81a526cc26e28d875cfe65d665c6ec0c8
j_pc <997282173@qq.com>
2020-04-07 21:46:58 +0800

    ac: 39

--- 39.组合总和.js
+++ 39.组合总和.js
@@ -11,7 +11,7 @@
  * @return {number[][]}
  */
 var combinationSum = function (candidates, target) {
-    let local_candidates = candidates.sort().filter(i => i <= target);
+    let local_candidates = candidates.sort((a, b) => a - b).filter(i => i <= target);
     let res = [];
     getResolve(target, local_candidates, res);
     return res;
@@ -22,7 +22,7 @@ function getResolve(target, candidates, res, cur = { list: [], total: 0 }, curIn
     for (let i = curIndex; i < candidates.length; i++) {
         const number = candidates[i];
         let value = cur.total + number;
-        let arr = [...cur.list, number].sort();
+        let arr = [...cur.list, number].sort((a, b) => a - b);
         if (value === target) {
             res.push(arr);
             return;
@@ -34,7 +34,17 @@ function getResolve(target, candidates, res, cur = { list: [], total: 0 }, curIn
     }
 }
 
-// console.log(combinationSum([2, 3, 6, 7], 7));
+// console.log(combinationSum([3, 12, 9, 11, 6, 7, 8, 5, 4], 15));
 // console.log(combinationSum([2, 3, 5], 8));
 // @lc code=end
 
+// [
+//     [3, 3, 3, 3, 3], [3, 3, 3, 6],
+//     [3, 3, 4, 5], [3, 3, 9],
+//     [3, 4, 4, 4], [3, 4, 8],
+//     [3, 5, 7], [3, 6, 6],
+//     [3, 12], [4, 4, 7],
+//     [4, 5, 6], [4, 11],
+//     [5, 5, 5], [6, 9],
+//     [7, 8]
+// ]
