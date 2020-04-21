ead869581cf5cc54b97c6b110fee8ea353cadd2c
ZHOUJIE <zhoujie01@souche.com>
2020-04-21 10:41:11 +0800

    AC: 51

---
+++ 51.n皇后.js
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode.cn id=51 lang=javascript
+ *
+ * [51] N皇后
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {string[][]}
+ */
+var solveNQueens = function (n) {
+    let matrix = createMatrix(n);
+    let solutions = [];
+    findSolution(matrix, solutions);
+    return solutions;
+};
+
+function findSolution(matrix, solutions, q_index = 0) {
+    let n = matrix.length;
+    if (q_index === n) {
+        solutions.push(matrix.map((i) => {
+            return i.join('');
+        }));
+        return;
+    }
+    for (let i = 0; i < n; i++) {
+        if (matrix[q_index][i] === 0) {
+            let new_matrix = copyMatrix(matrix);
+            // 可放置皇后
+            let res = setQueen(new_matrix, q_index, i);
+            if (res) {
+                findSolution(new_matrix, solutions, q_index + 1);
+            }
+        }
+    }
+}
+
+function copyMatrix(m) {
+    let n_m = [];
+    for (let i = 0; i < m.length; i++) {
+        n_m[i] = [];
+        for (let j = 0; j < m[i].length; j++) {
+            n_m[i][j] = m[i][j];
+        }
+    }
+    return n_m;
+}
+
+function createMatrix(n, ceil = 0) {
+    return (new Array(n)).fill('').map((i) => { return new Array(n).fill(ceil) })
+}
+
+function setQueen(matrix, n, m) {
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            if (i === n || j === m || i + j === m + n || i - j === n - m) {
+                // 遇到其他皇后
+                if (matrix[i][j] === 'Q') {
+                    return false;
+                }
+
+                if (i === n && j === m) {
+                    matrix[i][j] = 'Q';
+                } else {
+                    matrix[i][j] = '.';
+                }
+            }
+        }
+    }
+    return true;
+}
+// @lc code=end
+
+// let res = solveNQueens(4);
+// console.log(res);
