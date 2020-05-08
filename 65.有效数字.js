3b1c1ffb8097823923cd5d0d1afff068d4271c43
jeiizou <997282173@qq.com>
2020-05-08 21:56:22 +0800

    AC: 65

--- 65.有效数字.js
+++ 65.有效数字.js
@@ -10,84 +10,38 @@
  * @return {boolean}
  */
 var isNumber = function (s) {
-    // base case
-    if (s.length < 1) {
-        return false;
+    let state = 0,
+        finals = [0, 0, 0, 1, 0, 1, 1, 0, 1],
+        transfer = [[0, 1, 6, 2, -1, -1],
+        [-1, -1, 6, 2, -1, -1],
+        [-1, -1, 3, -1, -1, -1],
+        [8, -1, 3, -1, 4, -1],
+        [-1, 7, 5, -1, -1, -1],
+        [8, -1, 5, -1, -1, -1],
+        [8, -1, 6, 3, 4, -1],
+        [-1, -1, 5, -1, -1, -1],
+        [8, -1, -1, -1, -1, -1]],
+        make = (c) => {
+            switch (c) {
+                case " ": return 0;
+                case "+":
+                case "-": return 1;
+                case ".": return 3;
+                case "e": return 4;
+                default:
+                    let code = c.charCodeAt();
+                    if (code >= 48 && code <= 57) {
+                        return 2;
+                    } else {
+                        return 5;
+                    }
+            }
+        };
+    for (let i = 0; i < s.length; ++i) {
+        state = transfer[state][make(s[i])];
+        if (state < 0) return false;
     }
-
-    // trim
-    s = s.replace(/^\s+|\s+$/gm, '')
-
-    let length = 0;
-    for (let i = 0; i < s.length; i++) {
-        const num = s[i];
-        length++;
-        switch (num) {
-            case (num.match(/[0-9]/) || {}).input:
-                break;
-            case '.':
-            case 'e':
-            case '+':
-            case '-':
-            default:
-                return false
-        }
-    }
-
-    if (length === 0) {
-        return false;
-    }
-
-    return true;
+    return finals[state];
 };
 
-/**
- * 是否为整数
- */
-function isInt(str) {
-    if (str.match(/[0-9]/)) {
-        return true
-    } else {
-        return false
-    }
-}
-
-function dealNumber(str, opt = {
-    allowEmpty: true, //是否允许为空
-    allowDec: true, // 是否允许小数点
-    allowE: true,  // 是否允许指数E
-    allowSign: true, // 是否允许符号
-}) {
-
-}
 // @lc code=end
-
-// console.log(isNumber("01212"));
-// console.log(isNumber(" 0.1 "));
-// console.log(isNumber("abc"));
-// console.log(isNumber("1 a"));
-// console.log(isNumber("2e10"));
-// console.log(isNumber(" -90e3   "));
-// console.log(isNumber(" 1e"));
-// console.log(isNumber("e3"));
-// console.log(isNumber(" 6e-1"));
-// console.log(isNumber(" 99e2.5-1"));
-// console.log(isNumber("53.5e93"));
-// console.log(isNumber(" --6 "));
-// console.log(isNumber("-+3"));
-// console.log(isNumber("95a54e53"));
-// console.log(isNumber("   "));
-
-// console.log(isNumber(".1"));
-// console.log(isNumber("3."));
-// console.log(isNumber("3.1"));
-// console.log(isNumber("1 "));
-
-// console.log(isNumber("1 4"));
-// console.log(isNumber("."));
-// console.log(isNumber("   "));
-// console.log(isNumber("95a54e53"));
-// console.log(isNumber(" --6 "));
-// console.log(isNumber("-+3"));
-
-console.log(isNumber("11.8e1"));
