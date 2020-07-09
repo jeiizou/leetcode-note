633afca9da1a45a4461cc5ff32d1d9f5e78512f6
jeiizou <997282173@qq.com>
2020-07-09 23:04:53 +0800

    feay: 95

--- 95.不同的二叉搜索树-ii.js
+++ 95.不同的二叉搜索树-ii.js
@@ -17,8 +17,46 @@
  * @param {number} n
  * @return {TreeNode[]}
  */
-var generateTrees = function(n) {
-    let arr = new Array()
+var generateTrees = function (n) {
+    let n_arr = Array.from(Array(n), (v, k) => k + 1);
+    let res_arr = [];
+
+    let geneNode = (cur, arr, resLength) => {
+
+        if (!arr.length) {
+            if (resLength === 0) {
+                res_arr.push()
+            }
+            return null;
+        }
+
+        let right_arr = [];
+        let left_arr = [];
+        for (let i = 0; i < arr.length; i++) {
+            const node = arr[i];
+            if (node < cur) {
+                left_arr.push(node)
+            } else {
+                right_arr.push(node)
+            }
+        }
+
+        let leftTreeArr = []
+        for (let i = 0; i < left_arr.length; i++) {
+            const leftNode = left_arr[i];
+            let leftTree = geneNode(leftNode, [...left_arr.slice(0, i), left_arr.slice(i + 1)], resLength - 1);
+            leftTreeArr.push(leftTree);
+        }
+
+        let rightTreeArr = [];
+        for (let i = 0; i < right_arr.length; i++) {
+            const rightNode = right_arr[i];
+            let rightTree = geneNode(rightNode, [...right_arr.slice(0, i), right_arr.slice(i + 1)], resLength - 1);
+            rightTreeArr.puhs(rightTree)
+        }
+    }
 };
 // @lc code=end
 
+
+console.log(generateTrees(3))
