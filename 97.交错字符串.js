833aaa93ec8ac700fd517644a0a44db80ea763d6
ZHOUJIE <zhoujie01@souche.com>
2020-07-10 20:26:24 +0800

    add

---
+++ 97.交错字符串.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=97 lang=javascript
+ *
+ * [97] 交错字符串
+ */
+
+// @lc code=start
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @param {string} s3
+ * @return {boolean}
+ */
+var isInterleave = function (s1, s2, s3) {
+    let i = j = 0;
+
+
+
+    console.log(s3.filter(i => i).join(''));
+};
+
+function getStrArr(s3, s1) {
+
+    let i = 0;
+    while()
+}
+// @lc code=end
+
+// console.log(isInterleave('aabcc', 'dbbca', 'aadbbcbcac'));
+console.log(getStrArr('aadbbcbcac', 'aabcc'));
+
