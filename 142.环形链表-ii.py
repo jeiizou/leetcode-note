3e82d82e8cdb2ab22aab6b4a9e4e8b9ef28f9bf1
jeiizou <zhoujie01@souche.com>
2021-03-19 17:10:41 +0800

    feat: 环形链表

---
+++ 142.环形链表-ii.py
@@ -0,0 +1,38 @@
+#
+# @lc app=leetcode.cn id=142 lang=python3
+#
+# [142] 环形链表 II
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: ListNode) -> ListNode:
+        if not head:
+            return None
+        # 快慢执政
+        fast = slow = head
+        while fast:
+            slow = slow.next
+            # 快指针每次走两步
+            if fast.next:
+                fast = fast.next.next
+            else:
+                return None
+            # 如果快指针赶上了满指针
+            # 推论在于: 相遇点到入环点的距离 + (n-1) * 环长 = 链表头到入环点的距离
+            if fast == slow:
+                ptr = head
+                while ptr != slow:
+                    ptr = ptr.next
+                    slow = slow.next
+                return ptr
+        return None
+
+
+# @lc code=end
